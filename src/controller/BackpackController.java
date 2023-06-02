55fff353e110f60c1179cbd3d2c8dccf212b1b0d
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-06-02 16:12:42 +0900

    fix : 주석

--- src/controller/BackpackController.java
+++ src/controller/BackpackController.java
@@ -16,27 +16,27 @@ import view.output.ResultView;
 
 public class BackpackController {
     //View
-    private final ItemInputView itemInputView = new ItemInputView();                                //Item을 입력 받는 View
+    private final ItemInputView itemInputView = new ItemInputView();                                //Item 을 입력 받는 View
     private final NumberOfItemInputView numberOfItemInputView = new NumberOfItemInputView();        //처음 아이템 입력을 띄워주는 뷰
     private final BackpackInputView backpackInputView = new BackpackInputView();                    //배낭의 크기를 입력 받는 View
     private final ResultView resultView = new ResultView();                                         //결과를 보여주는 View
 
     //Service
     private final BackpackCalculateService calculateService = new BackpackCalculateService();       //입력된 것을 바탕으로 계산하는 Service
-    private final ItemDuplicateValidation itemDuplicateValidation = new ItemDuplicateValidation();  //Item이 중복되는지 검사하는 Service
+    private final ItemDuplicateValidation itemDuplicateValidation = new ItemDuplicateValidation();  //Item 이 중복되는지 검사하는 Service
 
     public void calculateStart() {
         //input
         NumberOfItemResponse numberOfItemResponse =
                 numberOfItemInputView.getNumberOfItem();                                            //Item 몇 개를 입력 받아야 하는지 입력 받는다
         ItemInputResponse itemInputResponse =
-                itemInputView.input(ItemInputRequest.from(numberOfItemResponse));                   //Item의 정보를 입력 받는다
+                itemInputView.input(ItemInputRequest.from(numberOfItemResponse));                   //Item 의 정보를 입력 받는다
         BackpackInputResponse backpackInputResponse =
                 backpackInputView.input();                                                          //배낭의 정보를 입력 받는다
 
         //service
         itemDuplicateValidation.check(itemInputResponse);                                           //입력된 정보를 바탕으로 중복이 있는지 검사한다
-        CalculateResponse calculateResponse =                                                       //계산을 한 후 Dto로 매핑한다
+        CalculateResponse calculateResponse =                                                       //계산을 한 후 Dto 로 매핑한다
                 calculateService.calculate(
                         CalculateRequest.from(itemInputResponse, backpackInputResponse)
                 );
